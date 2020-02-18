fb3a2c812112cba2c3355bf71185c57cfb70d385
Rahul <rahul.shinde@digitas.com>
2020-02-18 18:25:26 +0530

    Delete Book

--- booklist/src/Book.js
+++ booklist/src/Book.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
-
 export default class Book extends Component {
   render() {
-    const { img, title, author } = this.props.info;
+    const { id, img, title, author } = this.props.info;
+    const { handleDelete } = this.props;
+    console.log(id)
+
     return (
       <article className="book">
         <img src={img} width="150" alt="book" />
         <div>
           <h4>Title : {title}</h4>
           <h6>Author : {author}</h6>
+          <button type="button" onClick={() => handleDelete(id)}>Delete me</button>
         </div>
       </article>
     );
--- booklist/src/Booklist.js
+++ booklist/src/Booklist.js
@@ -9,14 +9,19 @@ export default class Booklist extends Component {
             books: booksData
           };
     }
+
+    handleDelete = (id) => {
+        const sortedBooks = this.state.books.filter((item) => item.id !== id)
+        this.setState({books: sortedBooks})
+    }
     
     render() {
-        
+        // console.log('>>>', this.state.books)
         return (
             <section>
                 <h3>This is our Booklist</h3>
                 {this.state.books.map(item => (
-                    <Book key={item.id} info={item} />
+                    <Book key={item.id} info={item} handleDelete={this.handleDelete} />
                 ))}
             </section>
         )
