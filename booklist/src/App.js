fabe1db02dd089e9c3218b59c69009e2770ffb5c
Rahul <rahul.shinde@digitas.com>
2020-02-18 16:55:10 +0530

    setup with initial display

---
+++ booklist/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import BookList from './BookList';
+import "./App.css";
+
+const App = () => (
+  <section>
+    <BookList />
+  </section>
+)
+
+export default App;
---
+++ booklist/src/Book.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class Book extends Component {
+  render() {
+    const { img, title, author } = this.props.info;
+    return (
+      <article className="book">
+        <img src={img} width="150" alt="book" />
+        <div>
+          <h4>Title : {title}</h4>
+          <h6>Author : {author}</h6>
+        </div>
+      </article>
+    );
+  }
+}
