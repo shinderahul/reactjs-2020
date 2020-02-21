7d38d50adc02743bad501654810eef1b41ddd1f0
Rahul <rahul.shinde@digitas.com>
2020-02-21 18:30:42 +0530

    City Tour setup

---
+++ city-tours/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return <h1>our current app</h1>;
+  }
+}
+
+export default App;
---
+++ city-tours/src/tourData.js
@@ -0,0 +1,35 @@
+export const tourData = [
+    {
+      id: 1,
+      city: "Mumbai",
+      img: "./img/mumbai.jpeg",
+      name: "Amachi Mumbai tour",
+      info:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+    },
+    {
+      id: 2,
+      city: "Delhi",
+      img: "./img/delhi.jpeg",
+      name: "Delhi tour",
+      info:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+    },
+    {
+      id: 3,
+      city: "Kolkata",
+      img: "./img/kolkata.jpeg",
+      name: "Kolkata bridge tour",
+      info:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+    },
+    {
+      id: 4,
+      city: "Chennai",
+      img: "./img/chennai.jpeg",
+      name: "Chennai beach tour",
+      info:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+    }
+  ];
+  
