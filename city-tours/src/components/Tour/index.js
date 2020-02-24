2b53d87f4b55d75033c0bcff25bb357186b38a59
Rahul <rahul.shinde@digitas.com>
2020-02-24 14:34:34 +0530

    City Tour | listed data from tour component

--- city-tours/src/components/Tour/index.js
+++ city-tours/src/components/Tour/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 
 export default class Tour extends Component {
+    
     render() {
+        console.log(this.props)
         return (
             <div>
                 Hello from TOUR
--- city-tours/src/components/TourList/index.js
+++ city-tours/src/components/TourList/index.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
-import Tour from '../Tour'
+import Tour from '../Tour';
+import "./tourlist.scss";
+import { tourData } from '../../tourData';
 
 export default class TourList extends Component {
+    state = {
+        tours: tourData
+    };
     render() {
+        const {tours} = this.state;
         return (
-            <div>
-                Hello from TOURLIST
-                <Tour />
-            </div>
+            <section className="tourlist">
+                {tours.map(tour => (
+                    <Tour key={tour.id} tour={tour} />
+                ))}
+            </section>
         )
     }
 }
