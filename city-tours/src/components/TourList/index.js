57b6e0f92ec837cc5dcd81f94cb58c29205b762d
Rahul <rahul.shinde@digitas.com>
2020-02-24 14:48:11 +0530

    City Tour | Delete city

--- city-tours/src/components/TourList/index.js
+++ city-tours/src/components/TourList/index.js
@@ -7,12 +7,20 @@ export default class TourList extends Component {
     state = {
         tours: tourData
     };
+    removeTour = id => {
+        console.log(id)
+        const sortedTour = this.state.tours.filter((item) => (
+            item.id !== id
+        ));
+        this.setState({tours: sortedTour})
+    }
+
     render() {
         const {tours} = this.state;
         return (
             <section className="tourlist">
                 {tours.map(tour => (
-                    <Tour key={tour.id} tour={tour} />
+                    <Tour key={tour.id} tour={tour} removeTour={this.removeTour} />
                 ))}
             </section>
         )
