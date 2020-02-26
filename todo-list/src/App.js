35ef8d2531e14d6cd1bd35fdeb90368e12bcff08
Rahul <rahul.shinde@digitas.com>
2020-02-26 12:57:39 +0530

    Todo list | background color change

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import uuid, { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import TodoInput from './components/TodoInput';
@@ -59,7 +59,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <div className="container">
+        <div className="container" style={{backgroundColor: "#f8f8f8"}}>
           <div className="row">
           <div className="col-10 mx-auto col-md-8 mt-5">
             <h3 className="text-capitalize text-center">todo input</h3>
